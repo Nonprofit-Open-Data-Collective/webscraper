61d46077e77e9b794f538d2cf43d0dc51c90e917
youngjaewon <youngjaewon91@gmail.com>
2020-01-24 12:49:53 -0700

    Update get_redirected_url.R

--- R/get_redirected_url.R
+++ R/get_redirected_url.R
@@ -1,20 +1,20 @@
 get_redirected_url <- function( input.URL ){
         # Add a check if the URL value redirects. If so, that redirected portion is what needs to be pasted to the front of the outgoing link
-        URL.redirect <- cleanURLtoCompare( GET( input.URL )[ 1 ] )
+        URL.redirect <- cleanURLtoCompare( httr::GET( input.URL )[ 1 ] )
         
-        html_page <- read_html( URL.redirect )
+        html_page <- xml2::read_html( URL.redirect )
         # See if the website is contained within a frame. If so, extract the URL embedded within the frame 
         # Use that URL as the redirect value
         html_page %>%
-        rvest::html_nodes( "frame") %>%
-        rvest::html_attr( "src" ) -> frame.links
+                rvest::html_nodes( "frame") %>%
+                rvest::html_attr( "src" ) -> frame.links
         
         # Include some sort of check; if frame.links != length( 0 ), then should use that URL
         if( length( frame.links ) != 0 ){
                 # Definitely have it overwrite the html_page, need to wrap in read_html()
                 html_page <- read_html( frame.links[ !is.na( frame.links ) ] )
                 # Have this overwrite the redirected value
-                return( cleanURLtoCompare( GET( frame.links[ !is.na( frame.links ) ] )[1] ) )
+                return( cleanURLtoCompare( httr::GET( frame.links[ !is.na( frame.links ) ] )[1] ) )
         }
         else{
                 return( URL.redirect )       
