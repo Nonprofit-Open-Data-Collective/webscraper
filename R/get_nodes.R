d5ad07be1cd99e4ae4f0980c8088df7502856eeb
youngjaewon <youngjaewon91@gmail.com>
2020-01-30 19:39:32 -0700

    Update get_nodes.R

--- R/get_nodes.R
+++ R/get_nodes.R
@@ -1,10 +1,6 @@
 getNodes <- function( input.URL ){
     html_page <- xml2::read_html( input.URL )
     
-    ## Extract the URL portion after the primary domain, for comparisons within the loop
-    URL.tail <- gsub( "^https?:/{2}([[:alnum:]\\.]*/)", "", input.URL  )
-    
-    
     html_page %>%  # read in the site as an HTML file
       rvest::html_nodes( "a" ) %>% # return all nodes with links in them
       rvest::html_attr( "href" ) -> sample.links # pulls out hypertext reference, links to other pages
