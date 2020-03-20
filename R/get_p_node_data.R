b27c3a6515ef058aeb282c6a271f37f3dde682bc
youngjaewon <youngjaewon91@gmail.com>
2020-03-20 12:50:56 -0700

    update utils and get_p_node_data

--- R/get_p_node_data.R
+++ R/get_p_node_data.R
@@ -1,9 +1,12 @@
+#' @importFrom magrittr %>%
+
 get_p_node_data <- function( input.URL ){
+  input.URL <- get_redirected_url( input.URL )
   internal.links <- Rcrawler::LinkExtractor(url = input.URL)$InternalLinks
-  
+
   results.list <- NULL
   results.df <- NULL
-  
+
   node.list <- NULL
   xpath.list <- NULL
   URL.list <- NULL
@@ -11,22 +14,22 @@ get_p_node_data <- function( input.URL ){
   page.list <- NULL
   data.list <- NULL
   tag.list <- NULL
-  
+
   for ( i in 1:length(internal.links) ){
     node.list[[i]] <- xml2::read_html(internal.links[i]) %>% xml2::xml_find_all('//*/p')
     xpath.list[[i]] <- node.list[[i]] %>% xml2::xml_path()
     data.list[[i]] <- node.list[[i]] %>% xml2::xml_text()
-    
+
     URL.list[[i]] <- rep(internal.links[i], length(data.list[[i]]))
     domain.list[[i]] <- rep(input.URL, length(data.list[[i]]))
     page.list[[i]] <- rep(stringr::str_remove(internal.links[i], input.URL), length(data.list[[i]]))
     tag.list[[i]] <- rep("p", length(data.list[[i]]))
-    
+
     results.list[[i]] <- dplyr::bind_cols(list(URL=URL.list[[i]], domain=domain.list[[i]], page=page.list[[i]], xpath=xpath.list[[i]], data=data.list[[i]], tag=tag.list[[i]]))
   }
-  
+
   results.df <- dplyr::bind_rows( results.list )
-  
+
   return(results.df)
 }
 
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,49 @@
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
+
+## To clean the URLs for the purpose of intial downloads
+## Ensures that every url starts with the same kind of header
+clean_url <- function( name ){
+  temp <- tolower( name )
+  for( i in 1:length( temp ) ){
+    if( grepl( "^http:/{2}", temp [ i ] ) == T ){
+      temp[ i ] <- sub( "^http:/{2}", "", temp[ i ] ) # removes "http://"
+      if( grepl( "^www\\.*", temp[ i ] ) == TRUE ){
+        temp[ i ] <- paste0( "http://", temp[ i ] )
+      } else {
+        temp[ i ] <- paste0( "http://www.", temp[ i ] )
+      }
+    } else if( grepl( "^https:/{2}", temp [ i ] ) == T ){
+      temp[ i ] <- sub( "^https:/{2}", "", temp[ i ] ) # removes "https://"
+      if( grepl( "^www\\.*", temp[ i ] ) == TRUE ){
+        temp[ i ] <- paste0( "https://", temp[ i ] )
+      } else {
+        temp[ i ] <- paste0( "https://www.", temp[ i ] )
+      }
+    } else if( grepl( "^https?:/{2}", temp [ i ] ) != T ) {
+      temp[ i ] <- paste0( "http://", temp[ i ] )
+    }
+  }
+  temp <- gsub( "/$", "", temp ) # Remove trailing "/", important for initial URL generation
+  return( temp )
+}
+
+## To clean the names of URLs for the purpose of saving mirrors
+cleanName <- function( name ){
+  temp <- sub( "^https?:/{2}", "", name ) # removes "http://" and "https://"
+  temp <- sub( "^.*w{3}\\.", "", temp ) # removes "www."
+  temp <- gsub( "/$", "", temp ) # removes any "/" at the end
+  temp <- gsub( " ", "", temp ) # removes any " "
+  temp <- gsub( "\\?|:", "", temp ) # removes any "\" or ":"
+  temp <- gsub( "/", "_._", temp  ) # converts any "/" within the address to a representative string, "_._"
+  return( temp )
+}
+
+## To clean the URLs for the purpose of comparing and removing duplicates
+cleanURLtoCompare <- function( name ){
+  temp <- gsub( " ", "", name ) # removes any " "
+  temp <- gsub( "\\>/{2}", "/", temp ) # converts any instances of "//" to "/" after the http(s):// head text
+  temp <- gsub( "/$", "", temp ) # removes any "/" at the end
+  return( temp )
+}
