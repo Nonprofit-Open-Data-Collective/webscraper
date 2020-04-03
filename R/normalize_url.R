7b8849d5dc2c3644926febe0fd4f6184745bdb2f
youngjaewon <youngjaewon91@gmail.com>
2020-04-03 12:01:44 -0700

    Update normalize_url.R

--- R/normalize_url.R
+++ R/normalize_url.R
@@ -4,10 +4,6 @@ normalize_url <- function( input.URL ){
   has_http_or_https <- grepl( "^https?:/{2}", temp )
   has_www <- grepl( "www\\.*" , temp)
     
-  if( has_http_or_https & has_www ){
-    temp <- paste0( "http://", temp ) 
-  }
-    
   if( has_http_or_https & !has_www){
     temp <- sub( "^https?:/{2}", "http://www.", temp )
   }
