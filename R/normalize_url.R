d2abecfda0fefaa75e7a1b372529b501d94099dd
youngjaewon <youngjaewon91@gmail.com>
2020-04-14 22:32:54 -0700

    Update normalize_url.R

--- R/normalize_url.R
+++ R/normalize_url.R
@@ -11,8 +11,8 @@ normalize_url <- function( input.URL ){
   
   # sort query parameters alphabetically
   # regex pattern from https://stackoverflow.com/questions/14679113/getting-all-url-parameters-using-regex
-  query_params <- stringr::str_extract_all( temp, "[^&?]*?=[^&?]*" ) %>%
-    unlist() %>% sort() %>% paste0( collapse="&" ) # sort query parameters
+  query_params <- stringr::str_extract_all( temp, "[^&?]*?=[^&?]*", simplify = TRUE ) %>%
+    sort() %>% paste0( collapse="&" ) # sort query parameters
   
   if( query_params != "" ){
     query_params <- paste0( "?", query_params )
@@ -22,7 +22,7 @@ normalize_url <- function( input.URL ){
   
   # avoid adding "www" in unnecessary cases (e.g. http://www.sites.google.com/view/gmfr)
   if( grepl("/", temp) ){ # check if url has slash(/)
-    string_before_first_slash <- stringr::str_extract_all( temp, "^(.*?)(?=/)") %>% unlist # extract string before the first '/'
+    string_before_first_slash <- stringr::str_extract_all( temp, "^(.*?)(?=/)", simplify = TRUE) # extract string before the first '/'
   } else{
     string_before_first_slash <- temp
   }
