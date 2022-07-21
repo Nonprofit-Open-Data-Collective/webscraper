67c1de29b1f7a4aa7653d0dea12fc3d6506f39c3
Blake Robert Mills <brm2143@columbia.edu>
2022-07-21 10:25:32 -0400

    New table function to take different lengths of inputs

---
+++ R/create_table.R
@@ -0,0 +1,37 @@
+#' @title
+#' Create a data frame of cleaned URLs
+#' @description
+#' Creates a data frame from a vector of inputted URLs that provides the cleaned, root, and redirected versions of the inputted URLs
+#' @param input String or vector of URLs. Must have a length greater than 1.
+#' @export
+
+
+## Currently Getting errors with issues in curl fetch
+
+create_table <- function( input ){
+
+  if( length( input ) < 1 ){
+
+    stop( "Must Input at Least 1 URL" )
+
+  }
+
+  if( length( input )== 1 ){
+
+    URL.frame <- create_table_01( input )
+
+  }else{
+
+    URL.frame <- data.frame()
+
+    for( i in 1:length( input ) ){
+      URL.table <- create_table_01( input[i] )
+      URL.frame <- rbind( URL.frame, URL.table )
+    }
+
+  }
+
+  return(URL.frame)
+}
+
+
