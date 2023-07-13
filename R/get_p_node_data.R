d94e59d00e34108995fb18f08f491ce0e089d584
Jesse Lecy <jdlecy@gmail.com>
2023-07-13 19:16:21 -0400

    Update get_p_node_data.R

--- R/get_p_node_data.R
+++ R/get_p_node_data.R
@@ -12,44 +12,61 @@
 #' input.URL <- "HTTP://GMFD.ORG/GMFRA/GMFRAINDEX.HTM"
 #' get_p_node_data( input.URL )
 #' @importFrom magrittr %>%
-
-get_p_node_data <- function( input.URL ){
+get_p_node_data <- function( input.URL )
+{
   redirected.URL <- get_redirected_url( input.URL )
-  internal.links <- Rcrawler::LinkExtractor(url = redirected.URL)$InternalLinks
+  internal.links <- Rcrawler::LinkExtractor( url = redirected.URL )$InternalLinks
 
-  results.list <- NULL
-  results.df <- NULL
+  results.list   <- NULL
+  results.df     <- NULL
 
-  input.URL.list <-NULL
-  node.list <- NULL
-  xpath.list <- NULL
-  URL.list <- NULL
-  domain.list <- NULL
-  page.list <- NULL
-  text.list <- NULL
-  tag.list <- NULL
+  input.URL.list <- NULL
+  node.list      <- NULL
+  xpath.list     <- NULL
+  URL.list       <- NULL
+  domain.list    <- NULL
+  page.list      <- NULL
+  text.list      <- NULL
+  tag.list       <- NULL
 
-  for ( i in 1:length(internal.links) ){
+  for ( i in 1:length(internal.links) )
+  {
 
-    if(httr::http_error( internal.links[i] ) == TRUE ){next}
+    if( httr::http_error( internal.links[i] ) == TRUE ){next}
 
-    node.list[[i]] <- xml2::read_html(internal.links[i]) %>% xml2::xml_find_all('//*/p')
+    node.list[[i]]  <- 
+      xml2::read_html(internal.links[i]) %>% 
+      xml2::xml_find_all('//*/p')
+
     xpath.list[[i]] <- node.list[[i]] %>% xml2::xml_path()
-    text.list[[i]] <- node.list[[i]] %>% xml2::xml_text()
+    text.list[[i]]  <- node.list[[i]] %>% xml2::xml_text()
 
-    input.URL.list[[i]] <- rep(input.URL, length(text.list[[i]]))
-    URL.list[[i]] <- rep(internal.links[i], length(text.list[[i]]))
-    domain.list[[i]] <- rep(redirected.URL, length(text.list[[i]]))
-    page.list[[i]] <- rep(stringr::str_remove(internal.links[i], redirected.URL), length(text.list[[i]]))
-    tag.list[[i]] <- rep("p", length(text.list[[i]]))
+    num.nodes <- length( text.list[[i]] )
 
-    results.list[[i]] <- dplyr::bind_cols(list(input.URL=input.URL.list[[i]], redirected.URL=domain.list[[i]], URL=URL.list[[i]], page=page.list[[i]], xpath=xpath.list[[i]], text=text.list[[i]], tag=tag.list[[i]]))
+    input.URL.list[[i]] <- rep( input.URL, num.nodes )
+    URL.list[[i]]       <- rep( internal.links[i], num.nodes )
+    domain.list[[i]]    <- rep( redirected.URL, num.nodes )
+    page.list[[i]]      <- rep( stringr::str_remove( internal.links[i], redirected.URL), num.nodes )
+    tag.list[[i]]       <- rep( "p", num.nodes )
+
+    results.list[[i]] <- 
+     dplyr::bind_cols( list( input.URL=input.URL.list[[i]], 
+                             redirected.URL=domain.list[[i]], 
+                             URL=URL.list[[i]], 
+                             page=page.list[[i]], 
+                             xpath=xpath.list[[i]], 
+                             text=text.list[[i]], 
+                             tag=tag.list[[i]] ) )
   }
 
-  results.df <- dplyr::bind_rows( results.list ) %>%
-    dplyr::mutate(text = stringr::str_squish( text )) %>%
-    dplyr::filter( nchar(text) > 0 )
+  df <- 
+    dplyr::bind_rows( results.list ) %>%
+    dplyr::mutate( text = stringr::str_squish( text ) ) %>%
+    dplyr::filter( nchar(text) > 0 ) %>% 
+    as.data.frame()
 
-  return( as.data.frame(results.df) )
+  df$page <- gsub( "^(/)(.*)(.[a-zA-Z]{3,4}$)", "\\2", df$page )
+
+  return( df )
 }
 
